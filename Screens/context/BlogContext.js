8abb82c6dfa85c64f65dfc7abaf5237396296d33
viswanath Reddy <viswanath3344@gmail.com>
2024-11-11 14:54:28 +0530

    Refactord useState with useReducer to centralize the Logic.

--- Screens/context/BlogContext.js
+++ Screens/context/BlogContext.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({children}) => {
-    const [posts, setPosts] = useState([]);
-    
-    const addPost = () => {
-        setPosts([...posts, {id: posts.length+1, title: `Post # ${posts.length+1}`, message: `Post Message #${posts.length+1}`}])
+const blogPostReducer = (posts, action) => {
+    switch (action.type) {
+        case 'add':
+            return [
+                ...posts,
+                {
+                    id: posts.length + 1,
+                    title: `Post # ${posts.length + 1}`,
+                    message: `Post Message #${posts.length + 1}`
+                }
+            ];
+        case 'default':
+            return posts
     }
-     return <BlogContext.Provider value={{data: posts, addPost}}>
-         {children}
-     </BlogContext.Provider>
+}
+
+export const BlogProvider = ({ children }) => {
+    const [posts, dispatch] = useReducer(blogPostReducer, [])
+
+    const addPost = () => {
+        dispatch({type: "add"})
+    }
+
+    return <BlogContext.Provider value={{ data: posts, addPost}}>
+        {children}
+    </BlogContext.Provider>
 };
 
-export default BlogContext ;
+export default BlogContext;
